b06cc6306eca4ef0855795030cedeecf3e27ec20
ZP045 <97619330+ZP045@users.noreply.github.com>
2022-02-07 16:17:20 -0300

    Create Genero.cs

---
+++ Enum/Genero.cs
@@ -0,0 +1,14 @@
+namespace DIO.Documentario
+{
+	public enum Genero
+	{
+		Natureza = 1,
+		Aventura = 2,
+		Espionagem = 3,
+		Piramides = 4,
+		Parques = 5,
+		Tecnologia = 6,
+		Agricultura = 7,
+                Restaurantes = 8,
+	}
+}
